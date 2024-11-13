d134c0547b014764aa55365405a9186b135a27f3
swarupSikder <swarupfx20@gmail.com>
2024-11-13 18:47:00 +0600

    day 7

---
+++ codechef_start160d/2.c
@@ -0,0 +1,41 @@
+//auther:   Swarup Sikder
+//judge:    codeChef
+//contest:  start 160d
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+void body(){
+    //---------------//
+    //     input     //
+    //---------------//
+    int n;
+    scanf("%d", &n);
+
+
+    //-----------------//
+    //     process     //
+    //-----------------//
+
+
+
+
+    //----------------//
+    //     output     //
+    //----------------//
+    printf("%d\n", n);
+}
+int main(){
+    //-------------------//
+    //     test case     //
+    //-------------------//
+    int t=1;
+    //scanf("%d", &t);
+
+    while(t--){
+        body();
+    }
+
+    return 0;
+}
